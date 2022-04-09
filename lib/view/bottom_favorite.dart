1d31723a9fce5d1d0c1d5a91d0ed6ef3c9fa5526
NguyenLoi <hongloi123123@gmail.com>
2022-04-09 16:08:43 +0700

    3. Create Bottom Navigation View

---
+++ lib/view/bottom_favorite.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BotttomFavorite extends StatefulWidget {
+  BotttomFavorite({Key? key}) : super(key: key);
+
+  @override
+  State<BotttomFavorite> createState() => _BotttomFavoriteState();
+}
+
+class _BotttomFavoriteState extends State<BotttomFavorite> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
