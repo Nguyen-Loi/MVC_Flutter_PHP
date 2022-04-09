ded8f683ad8393eae4da2463ebd43e9f854cca34
NguyenLoi <hongloi123123@gmail.com>
2022-04-09 15:51:29 +0700

    2. 3. Crate Connection to Get Ebook Data

---
+++ lib/controller/api.dart
@@ -0,0 +1,9 @@
+class ApiConstant {
+   String api = 'api.php?';
+  String slider = 'slider';
+  String latest = 'latest';
+  String category = 'category';
+  String baseUrl = 'http://localhost/ebookapp/';
+ 
+
+}
