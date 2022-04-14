e354307450b4ddc4b2f4722801a1abe5ec8426ce
NguyenLoi <hongloi123123@gmail.com>
2022-04-14 10:59:24 +0700

    9. Screate function choose image gallery and camera

--- lib/view/register/ebook_register.dart
+++ lib/view/register/ebook_register.dart
@@ -15,6 +15,76 @@ class EbookRegisterState extends State<EbookRegister> {
   File _file = File('');
   final imgPicker = ImagePicker();
 
+  void imagePicker(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return SafeArea(
+              child: Wrap(
+            children: [
+              ListTile(
+                leading: Icon(
+                  Icons.library_add,
+                  color: Colors.blue,
+                ),
+                title: Text(
+                  'Photo from Gallery',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                onTap: () {
+                  imageFromGallery();
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.camera,
+                  color: Colors.blue,
+                ),
+                title: Text(
+                  'Photo from camera',
+                  style: TextStyle(color: Colors.blue),
+                ),
+                onTap: () {
+                  imageFromCamera();
+                     Navigator.pop(context);
+                },
+              ),
+            ],
+          ));
+        });
+  }
+
+  imageFromGallery() async {
+    var imgFromGallery = await imgPicker.pickImage(
+        source: ImageSource.gallery,
+        imageQuality: 100,
+        maxHeight: 100,
+        maxWidth: 100);
+    setState(() {
+      if (imgFromGallery != null) {
+        _file = File(imgFromGallery.path);
+      } else {
+        print('This file dont have any data');
+      }
+    });
+  }
+
+  imageFromCamera() async {
+    var imgFromCamera = await imgPicker.pickImage(
+        source: ImageSource.camera,
+        imageQuality: 100,
+        maxHeight: 100,
+        maxWidth: 100);
+    setState(() {
+      if (imgFromCamera != null) {
+        _file = File(imgFromCamera.path);
+      } else {
+        print('This file dont have any data');
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,27 +93,45 @@ class EbookRegisterState extends State<EbookRegister> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Text('Create your account right now', style: TextStyle(
-                color: Colors.black, fontSize: 20
-              ),),
+              Text(
+                'Create your account right now',
+                style: TextStyle(color: Colors.black, fontSize: 20),
+              ),
               GestureDetector(
-                onTap: (){},
+                onTap: () {
+                  imagePicker(context);
+                },
                 child: Container(
                   margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
                   width: 130,
                   height: 130,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle
-                  ),
+                  decoration: BoxDecoration(shape: BoxShape.circle),
                   child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Icon(Icons.account_circle_outlined),
-                  ),
+                      borderRadius: BorderRadius.circular(100),
+                      child: _file.path == ''
+                          ? Image.asset(
+                              'asset/images/register.png',
+                              width: 30,
+                              height: 30,
+                              fit: BoxFit.cover,
+                            )
+                          : Image.file(
+                              _file,
+                              width: 30,
+                              height: 30,
+                              fit: BoxFit.cover,
+                            )),
                 ),
               ),
-             TextFiledMain(hintText: 'Enter your name', iconFiled: Icons.account_circle_outlined),
-             TextFiledMain(hintText: 'Enter your email', iconFiled: Icons.email_outlined),
-             TextFiledMain(hintText: 'Enter your password', iconFiled: Icons.lock_outline),
+              TextFiledMain(
+                  hintText: 'Enter your name',
+                  iconFiled: Icons.account_circle_outlined),
+              TextFiledMain(
+                  hintText: 'Enter your email',
+                  iconFiled: Icons.email_outlined),
+              TextFiledMain(
+                  hintText: 'Enter your password',
+                  iconFiled: Icons.lock_outline),
             ],
           ),
         ),
@@ -51,4 +139,3 @@ class EbookRegisterState extends State<EbookRegister> {
     );
   }
 }
-
